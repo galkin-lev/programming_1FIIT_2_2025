0950ec22d288c296b7c419b8d336d5b75ed2fa77
galkin-lev <galkinlev.v@gmail.com>
2025-10-09 15:48:11 +0200

    lab4

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "ru");
-    srand(time(0));
+    // srand(time(0));
     int n = 10;
-    int secret_number = rand() % n + 1;
+    int secret_number = 5;//rand() % n + 1;
     int guess, attempts = 0;
 
     cout << "Я загадал число от 1 до " << n << ". Угадай его!" << endl;
@@ -26,7 +26,19 @@ int main() {
         }
     } while (guess != secret_number);
 
-    cout << "Точно! Ты угадал за " << attempts << " попыток!" << endl;
+
+   /*  последняя цифра - 1 - попытку
+        2, 3, 4, - попытки
+        5 - 0 - попыток */
+    if (attempts % 10 == 1) {
+         cout << "Точно! Ты угадал за " << attempts << " попытку!" << endl;
+
+    } else if (attempts % 10 == 2 || attempts % 10 == 3 || attempts % 10 == 4) {
+            cout << "Точно! Ты угадал за " << attempts << " попытки!" << endl;
+
+    } else {
+        cout << "Точно! Ты угадал за " << attempts << " попыток!" << endl;
+    }
 
 
 
