15bde45adbf5190f4beda527d0ac98212b9bf8f7
galkin-lev <galkinlev.v@gmail.com>
2025-10-16 22:27:52 +0200

    1-3

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -8,6 +8,9 @@
 
 #include <algorithm>
 #include <string>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -83,21 +86,43 @@ bool compare(int a, int b) { // compare function for sort
     return a < b;
 }
 
+// 3 exercise
+int findMaxIndex(int arr[], int size) {
+    if (size < 0) return -1; // Handle empty array
+
+    int maxIndex = 0;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < arr[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
 int main() {
+
+    /*
+    #include <cstdlib>
+    #include <ctime>
+     */
+    srand(time(0)); // Initialize random number generator
+    int random_number_range = 10;
+    int randomNumber = rand() % random_number_range + 1;
+    cout << "Secret number range: " << randomNumber << endl;
 
 
     int tasknum;
 
     while (true) {
-        cout << "Enter the exercise (1-5): ";
+        cout << "Enter the exercise (1-4) or press any key to exit: ";
         cin >> tasknum;
+
+
+
+              // var 1
+/*1.  Дана последовательность натуральных чисел {aj}j=1...n (n<=10000). Если в последовательности есть хотя
+ *бы одно число, начинающееся цифрой 1, упорядочить последовательность по неубыванию.*/
         if (tasknum == 1) { // 1 задание
-
-
-
-            // var 1
-            /*1.  Дана последовательность натуральных чисел {aj}j=1...n (n<=10000). Если в последовательности есть хотя
-             *бы одно число, начинающееся цифрой 1, упорядочить последовательность по неубыванию.*/
 
             unsigned int n;
             while ( true  ) {
@@ -122,7 +147,7 @@ int main() {
 
                     cin >> sequence[i];
 
-                    if ( cin.fail() || sequence[i] <= 0) { // Проверка, что число натуральное (положительное)
+                    if ( cin.fail() || sequence[i] < 0) { // Проверка, что число натуральное (положительное)
                         cin.clear(); // Clear error flag
                         cin.ignore( numeric_limits< streamsize>::max(), '\n'); // Discard bad input
                         cout << "Invalid input. Please enter a valid number.\n";
@@ -185,7 +210,7 @@ int main() {
 
                     cin >> sequence[i];
 
-                    if ( cin.fail() || sequence[i] <= 0) { // Проверка, что число натуральное (положительное)
+                    if ( cin.fail() || sequence[i] < 0) { // Проверка, что число натуральное (положительное)
                         cin.clear(); // Clear error flag
                         cin.ignore( numeric_limits< streamsize>::max(), '\n'); // Discard bad input
                         cout << "Invalid input. Please enter a valid number.\n";
@@ -207,13 +232,17 @@ int main() {
 
         }
 
+
+/*
+ * 3.  Дана целочисленная матрица {Aij}i=1..n,j=1..m (n,m<=100). Найти строку, сумма элементов которой наиболее близка к 0,
+ * и заменить все элементы этой строки числом 0.
+ */
         if ( tasknum == 3 ) {
             
-            cout << "Enter sequence size (n) (n<=10000) : ";
             unsigned int n;
             unsigned int m;
             while ( true  ) {
-                cout << "Enter sequence size (n) (n<=10000) strings : ";
+                cout << "Enter matrix size (n) (n<=100) rows : ";
                 cin >> n;
 
                 if ( cin.fail() || n > 100 ) {
@@ -225,7 +254,7 @@ int main() {
                 }
             }
             while ( true  ) {
-                cout << "Enter sequence size (m) (m<=10000) columns : ";
+                cout << "Enter matrix size (m) (m<=100) columns : ";
                 cin >> m;
 
                 if ( cin.fail() || m > 100 ) {
@@ -239,7 +268,7 @@ int main() {
 
             
             int matrix[matrixDefaultSize][matrixDefaultSize];
-            int sums[100];
+            int sums[10000];
 
             // fill the matrix
             for (int i = 0; i < n; ++i) {
@@ -252,7 +281,7 @@ int main() {
                     cin >> matrix[i][j];
 
                      sums[i] += matrix[i][j];
-                    if ( cin.fail() || matrix[i][j] <= 0) { // Проверка, что число натуральное (положительное)
+                    if ( cin.fail() || matrix[i][j] < 0) { // Проверка, что число натуральное (положительное)
                         cin.clear(); // Clear error flag
                         cin.ignore( numeric_limits< streamsize>::max(), '\n'); // Discard bad input
                         cout << "Invalid input. Please enter a valid number.\n";
@@ -263,39 +292,29 @@ int main() {
                 }
             }
 
-            cout << "entered sequence:" << endl;
+            cout << "entered matrix:" << endl;
             printMatrix(matrix, n, m);
 
-            // // Вывод с индексами столбцов
-            //
-            // cout << "   ";  // отступ для строки с индексами
-            // for (int j = 0; j < m; j++) {
-            //     cout << setw(4) << "[" << j << "]";
-            // }
-            // cout << endl;
-            //
-            // // Вывод данных с индексами строк
-            // for (int i = 0; i < n; i++) {
-            //     cout << "[" << i << "]";
-            //     for (int j = 0; j < m; j++) {
-            //         cout << setw(5) <<  matrix[i][j]; // setw(
-            //     }
-            //     cout << endl;
-            // }
-            // cout << " of size: "  << endl;
+            cout << "row with the lowest sum value: " << findMaxIndex(sums, n) << endl;
+
+            cout << "the matrix with the nullified row" << endl;
+            for (int i = 0; i < m; ++i) {
+                matrix[findMaxIndex(sums, n)][i] = 0;
+            }
+            printMatrix(matrix, n, m);
 
 
 
         }
+        if (tasknum == 4) {
+
+        }
+        if (tasknum != 1 && tasknum != 2 && tasknum != 3 && tasknum != 4) {
+            cout << "exiting..." << endl;
+            return 0;
+        }
+
 
     }
 
-
-
-
-
-
-
-
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
